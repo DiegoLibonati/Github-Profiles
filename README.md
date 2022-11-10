1b4fd3947a6397d0693de71e653b9ed49625a657
DiegoLibonati <diego.libonati1998@gmail.com>
2022-11-09 21:38:45 -0300

    New html semantic, css fix, js fix

--- README.md
+++ README.md
@@ -17,46 +17,6 @@
 
 I made a web page that allows the user to search for github profiles. If you find a profile it will show: its profile image, its user, its description, how many followers it has, how many people it follows and how many repos it has. And it will make a list of buttons with each repo, also if you click on any repo it will take you to the official link of that repository in github. If it doesn't find it, it will launch an alert.
 
-## Feel free to edit my code
-
-I use api github to find the profile name.
-
-```
-let apiPetition =  await fetch(`https://api.github.com/users/${profile}`);
-```
-
-Here i create the html in javascript and called with a function:
-
-```
-const createHTMLWithOutRepositories = (img, name, description, followers, following, reposCount) => {
-
-    return `
-
-        <div class="section_container_card-img">
-            <img src="${img}" alt="${name}">
-        </div>
-
-        <div class="section_container_card-information">
-
-            <h2>${name}</h2>
-
-            <p>${description}</p>
-
-            <div class="card-followers">
-
-                <h3><span>${followers}</span>Followers</h3>
-                <h3><span>${following}</span>Following</h3>
-                <h3><span>${reposCount}</span>Repos</h3>
-
-            </div>
-
-        </div>
-
-    `;
-
-}
-```
-
 ## Technologies used
 
 1. Javascript
@@ -77,6 +37,4 @@ github
 
 ## Video
 
-
 https://user-images.githubusercontent.com/99032604/198900756-5b0d19d8-5779-47ed-b0fe-d27ef1a4c37d.mp4
-
--- codigo.js
+++ codigo.js
@@ -1,85 +1,95 @@
 const containerCard = document.querySelector(".section_container_card");
-const inputSearchProfile = document.querySelector(".section_container_search input");
-const buttonSearchProfile = document.querySelector(".section_container_search button");
+const inputSearchProfile = document.querySelector(
+  ".section_container_search input"
+);
+const buttonSearchProfile = document.querySelector(
+  ".section_container_search button"
+);
 
-buttonSearchProfile.addEventListener("click", (e)=>{
+buttonSearchProfile.addEventListener("click", (e) => {
+  let inputSearchProfileValue = inputSearchProfile.value;
 
-    let inputSearchProfileValue = inputSearchProfile.value;
+  let githubProfileJson = getGithubProfile(inputSearchProfileValue);
 
-    let githubProfileJson = getGithubProfile(inputSearchProfileValue);
+  githubProfileJson.then((res) => {
+    let githubProfileValues = checkIfBioIsNull(
+      res.avatar_url,
+      res.login,
+      res.bio,
+      res.followers,
+      res.following,
+      res.public_repos
+    );
 
-    githubProfileJson.then(res => {
-        
-        let githubProfileValues = checkIfBioIsNull(res.avatar_url, res.login, res.bio, res.followers, res.following, res.public_repos);
+    let githubProfileReposUrl = getGithubProfileRepos(res.repos_url);
 
-        let githubProfileReposUrl = getGithubProfileRepos(res.repos_url);
+    githubProfileReposUrl.then((res) => {
+      containerCard.innerHTML = createHTMLWithOutRepositories(
+        githubProfileValues[0],
+        githubProfileValues[1],
+        githubProfileValues[2],
+        githubProfileValues[3],
+        githubProfileValues[4],
+        githubProfileValues[5]
+      );
 
-        githubProfileReposUrl.then(res =>{
-            
-            containerCard.innerHTML = createHTMLWithOutRepositories(githubProfileValues[0], githubProfileValues[1], githubProfileValues[2], githubProfileValues[3], githubProfileValues[4], githubProfileValues[5]);
-
-            createHTMLRepositories(res);
-            
-            console.log(res);
-        })
-    })
-
+      createHTMLRepositories(res);
+    });
+  });
 });
 
-const getGithubProfile = async (profile) =>{
+const getGithubProfile = async (profile) => {
+  let apiPetition = await fetch(`https://api.github.com/users/${profile}`);
 
-    let apiPetition =  await fetch(`https://api.github.com/users/${profile}`);
+  if (apiPetition.ok === false) {
+    console.log("The profile dosen´t exist");
+    document.querySelector(
+      ".header_section_h2"
+    ).innerHTML = `The profile dosen´t exist 😔`;
+    document.querySelector(".header_section_h2").classList.add("show-info");
 
-    if (apiPetition.ok === false){
-        console.log ("The profile dosen´t exist");
-        document.querySelector(".header_section_h2").innerHTML = `The profile dosen´t exist 😔`;
-        document.querySelector(".header_section_h2").classList.add("show-info");
+    setTimeout(() => {
+      document
+        .querySelector(".header_section_h2")
+        .classList.remove("show-info");
+    }, 2000);
+  } else {
+    let apiResult = await apiPetition.json();
+    document.querySelector(
+      ".header_section_h2"
+    ).innerHTML = `The profile exist ✅`;
+    document.querySelector(".header_section_h2").classList.add("show-info");
 
-        setTimeout(()=>{
+    setTimeout(() => {
+      document
+        .querySelector(".header_section_h2")
+        .classList.remove("show-info");
+    }, 2500);
 
-            document.querySelector(".header_section_h2").classList.remove("show-info");
-
-        },2000);
-
-
-    } else {
-
-        let apiResult = await apiPetition.json();
-        document.querySelector(".header_section_h2").innerHTML = `The profile exist ✅`;
-        document.querySelector(".header_section_h2").classList.add("show-info");
-
-        setTimeout(()=>{
-
-            document.querySelector(".header_section_h2").classList.remove("show-info");
-
-        },2000);
-
-        return apiResult;
-        
-
-    }
-
-}
+    return apiResult;
+  }
+};
 
 const getGithubProfileRepos = async (reposLink) => {
+  let apiPetition = await fetch(`${reposLink}`);
 
-    let apiPetition = await fetch(`${reposLink}`);
+  if (apiPetition.ok === false) {
+    console.log("The repos profile dosen´t exist");
+  } else {
+    let apiResult = await apiPetition.json();
+    return apiResult;
+  }
+};
 
-    if (apiPetition.ok === false){
-        console.log ("The repos profile dosen´t exist");
-
-    } else {
-
-        let apiResult = await apiPetition.json();
-        return apiResult;
-
-    }
-
-}
-
-const createHTMLWithOutRepositories = (img, name, description, followers, following, reposCount) => {
-
-    return `
+const createHTMLWithOutRepositories = (
+  img,
+  name,
+  description,
+  followers,
+  following,
+  reposCount
+) => {
+  return `
     
         <div class="section_container_card-img">
             <img src="${img}" alt="${name}">
@@ -102,63 +112,76 @@ const createHTMLWithOutRepositories = (img, name, description, followers, follow
         </div>
 
     `;
+};
 
-}
+const createHTMLRepositories = (repo) => {
+  const div = document.createElement("DIV");
+  const h3 = document.createElement("H3");
+  const ul = document.createElement("UL");
+  const containerCardInformation = document.querySelector(
+    ".section_container_card-information"
+  );
 
-const createHTMLRepositories = (repo) =>{
+  div.setAttribute("class", "card-repos");
+  ul.setAttribute("class", "card-repos-list");
 
-    const div = document.createElement("DIV");
-    const h3 = document.createElement("H3");
-    const ul = document.createElement("UL");
-    const containerCardInformation = document.querySelector(".section_container_card-information");
+  containerCardInformation.append(div);
+  div.append(h3);
+  div.append(ul);
 
-    div.setAttribute("class", "card-repos");
-    ul.setAttribute("class", "card-repos-list");
+  h3.innerHTML = "Repositories";
 
-    containerCardInformation.append(div);
-    div.append(h3);
-    div.append(ul);
-    
-    h3.innerHTML = "Repositories";
+  for (let i = 0; i < 10; i++) {
+    ul.innerHTML += `<li><a href=${repo[i].html_url} target="BLANK">${repo[i].name}</a></li>`;
+  }
+};
 
-    if (ul.children.length === 10){
-    } else {
-        for (let i = 0; i < repo.length; i++){
-            ul.innerHTML += `<li><a href=${repo[i].html_url} target="BLANK">${repo[i].name}</a></li>`;
-        }
-    }
+const checkIfBioIsNull = (
+  img,
+  name,
+  description,
+  followers,
+  following,
+  repos
+) => {
+  let profileImg;
+  let profileLogin;
+  let profileDescription;
+  let profileFollowers;
+  let profileFollowing;
+  let profilePublicRepos;
 
-}
+  if (description === null || description === undefined || description === "") {
+    profileImg = img;
+    profileLogin = name;
+    profileDescription = "There is not description in this profile, sorry 😔";
+    profileFollowers = followers;
+    profileFollowing = following;
+    profilePublicRepos = repos;
 
-const checkIfBioIsNull = (img, name, description, followers, following, repos) => {
+    return [
+      profileImg,
+      profileLogin,
+      profileDescription,
+      profileFollowers,
+      profileFollowing,
+      profilePublicRepos,
+    ];
+  } else {
+    profileImg = img;
+    profileLogin = name;
+    profileDescription = description;
+    profileFollowers = followers;
+    profileFollowing = following;
+    profilePublicRepos = repos;
 
-    let profileImg;
-    let profileLogin; 
-    let profileDescription; 
-    let profileFollowers; 
-    let profileFollowing;
-    let profilePublicRepos;
-
-    if (description === null || description === undefined || description === ""){
-
-        profileImg = img;
-        profileLogin = name;
-        profileDescription = "There is not description in this profile, sorry 😔";
-        profileFollowers = followers;
-        profileFollowing = following;
-        profilePublicRepos = repos;
-
-        return [profileImg, profileLogin, profileDescription, profileFollowers, profileFollowing, profilePublicRepos];
-    } else {
-
-        profileImg = img;
-        profileLogin = name;
-        profileDescription = description;
-        profileFollowers = followers;
-        profileFollowing = following;
-        profilePublicRepos = repos;
-
-        return [profileImg, profileLogin, profileDescription, profileFollowers, profileFollowing, profilePublicRepos];
-    }
-
-}
+    return [
+      profileImg,
+      profileLogin,
+      profileDescription,
+      profileFollowers,
+      profileFollowing,
+      profilePublicRepos,
+    ];
+  }
+};
